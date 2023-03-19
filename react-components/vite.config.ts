d9aaa62dcce2987af2d5702a2a88e913d8bdccc2
Svetlana <lanavladi1605gmail.com>
2023-03-20 03:41:51 +0500

    fix: set up environment

--- react-components/vite.config.ts
+++ react-components/vite.config.ts
@@ -4,12 +4,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'c8',
+      all: true,
+      skipFull: false,
+      reporter: 'text',
+    },
   },
 });
