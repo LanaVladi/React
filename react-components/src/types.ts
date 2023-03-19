35c8f70229ac2c97dcfe56857536e6ae8f43f314
Svetlana <lanavladi1605gmail.com>
2023-03-20 03:43:54 +0500

    feat: add types

---
+++ react-components/src/types.ts
@@ -0,0 +1,11 @@
+export interface DataPropsType {
+  id: string;
+  bookName: string;
+  author: string;
+  price: string;
+  publishingHouse: string;
+  quantityInStock: string;
+  image: string;
+  publicationYear: string;
+  isPopular: string;
+}
