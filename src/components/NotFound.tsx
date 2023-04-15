5ea45a3f5e448a06c52e4a41068ebc62eae006a6
Svetlana <lanavladi1605gmail.com>
2023-04-15 15:39:56 +0500

    feat: add NotFound component

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <>
+      <div className="card-not-found">
+        <h3>CARD NOT FOUND! PLEASE TRY AGAIN!</h3>
+      </div>
+    </>
+  );
+}
+
+export { NotFound };
