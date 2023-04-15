6dae73afe9110d24dea05ebc0757b8fa29eae4a7
Svetlana <lanavladi1605gmail.com>
2023-04-15 15:39:23 +0500

    feat: add LoadingIndicator component

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoadingIndicator() {
+  return (
+    <>
+      <div className="loading-indicator">
+        <h3>Data is loading...</h3>
+      </div>
+    </>
+  );
+}
+
+export { LoadingIndicator };
