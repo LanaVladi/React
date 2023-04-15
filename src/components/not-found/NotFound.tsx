4da9d285ec2167ab68dbb1aedf8054994b4b6ee4
Svetlana <lanavladi1605gmail.com>
2023-04-15 18:01:46 +0500

    refactor: NotFound component

---
+++ src/components/not-found/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './not-found.css';
+
+function NotFound() {
+  return (
+    <>
+      <div className="card-not-found">
+        <h3>CARD NOT FOUND! PLEASE TRY AGAIN!</h3>
+      </div>
+    </>
+  );
+}
+
+export { NotFound };
